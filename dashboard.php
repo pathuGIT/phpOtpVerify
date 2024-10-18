4f656687306924ba80e674ea016d94b4b25dbf54
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2024-10-18 12:27:25 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+    <h1>Welcome to the Dashboard!</h1>
+    <p><a href="logout.php">Logout</a></p>
+    <?php 
+        echo "Hello" . $_SESSION['temp_userName'];
+    ?>
+</body>
+</html>
